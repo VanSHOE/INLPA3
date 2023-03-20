03f6743bc1e463c02d49f4587afaf8d7a9b7ee4e
Rahul Garg <rahulgargofficial@gmail.com>
2023-03-21 03:14:01 +0530

    NN Fixes

--- nn4.py
+++ nn4.py
@@ -5,20 +5,23 @@ from pprint import pprint
 import random
 import time
 from sklearn.metrics import classification_report as cr
+from torch.autograd import Variable
 import os
 import json
 import pickle
 
 random.seed(time.time())
-
+logFile = open(f"logs/loss.txt", "w", encoding="utf8")
 sentenceLens = {}
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
-
+logFile.write(device + "\n")
+logFile.flush()
 CONTEXT = 3  # Left and right context hence total context is 2*CONTEXT
-sentenceLimit = 15000
+sentenceLimit = 10000
 BATCH_SIZE = 250
 MODEL = "Test"
+NEGSAMPLES = 1
 
 
 # CBOW
@@ -26,7 +29,8 @@ class Data(torch.utils.data.Dataset):
     def __init__(self, sentences):
         self.device = device
         self.context = CONTEXT
-        self.sentences = [sentence for sentence in sentences if len(sentence) > 2 * self.context]
+        self.sentences = [sentence for sentence in sentences if len(
+            sentence) > 2 * self.context]
         print(f"Sentences: {len(self.sentences)}")
         idxPtr = 0
         self.w2idx = {}
@@ -44,21 +48,42 @@ class Data(torch.utils.data.Dataset):
 
         for sentence in self.sentences:
             for i in range(self.context, len(sentence) - self.context):
-                self.inputs.append(sentence[i - self.context:i] + sentence[i + 1:i + self.context + 1])
+                self.inputs.append(
+                    sentence[i - self.context:i] + sentence[i + 1:i + self.context + 1])
                 self.outputs.append(sentence[i])
         # conver to idxs
-        self.inputs = [[self.w2idx[word] for word in sentence] for sentence in self.inputs]
+        self.inputs = [[self.w2idx[word] for word in sentence]
+                       for sentence in self.inputs]
         self.outputs = [self.w2idx[word] for word in self.outputs]
 
+        # neg sample input outputs
+        self.negInputs = []
+        self.negOutputs = []
+        # context, output_word -> 0 or 1 for all input output (one hot)
+        for i in range(len(self.inputs)):
+            for word in self.inputs[i]:
+                self.negInputs.append((word, self.outputs[i]))
+                self.negOutputs.append(1)
+            for j in range(NEGSAMPLES):
+                negWord = random.randint(0, self.vocab_size - 1)
+                while negWord in self.inputs[i]:
+                    negWord = random.randint(0, self.vocab_size - 1)
+                self.negInputs.append((negWord, self.outputs[i]))
+                self.negOutputs.append(0)
+
         # convert to tensor
-        self.inputs = torch.tensor(self.inputs).to(self.device)
-        self.outputs = torch.tensor(self.outputs).to(self.device)
+        self.negInputs = torch.tensor(self.negInputs).to(self.device)
+        self.negOutputs = torch.tensor(self.negOutputs).to(self.device)
+        print(f"Inputs: {len(self.negInputs)}")
 
     def __len__(self):
-        return len(self.inputs)
+        return len(self.negInputs)
 
     def __getitem__(self, idx):
-        return self.inputs[idx], self.outputs[idx]
+        oneHot = torch.zeros(self.vocab_size, device=self.device)
+        oneHot[self.negInputs[idx][0]] = 0.5
+        oneHot[self.negInputs[idx][1]] = 0.5
+        return oneHot, self.negOutputs[idx]
 
 
 class NN(nn.Module):
@@ -99,9 +124,9 @@ def train(model, data, optimizer, criterion):
 
     epoch = 0
     model.train_data = data
-    logFile = open(f"logs/loss.txt", "w", encoding="utf8")
+    pat = 5
     while lossDec:
-        torch.save(model.state_dict(), f"model.pt")
+        torch.save(model.state_dict(), f"modelFixed500_4.pt")
         epoch_loss = 0
         for i, (x, y) in enumerate(dataL):
             optimizer.zero_grad()
@@ -112,27 +137,42 @@ def train(model, data, optimizer, criterion):
             output = model(x)
 
             y = y.view(-1)
+            # output=output.view(-1)
             output = output.view(-1, output.shape[-1])
-
+            # print(output)
+            # output = [1.] * len(output)
+            # output = torch.tensor(output, requires_grad=True).to(model.device)
             loss = criterion(output, y)
+            # loss=torch.abs(output - y)
+            # loss=Variable(torch.mean(loss, 0), requires_grad=True)
+
             loss.backward()
             optimizer.step()
             epoch_loss += loss.item()
-            # if i % 100 == 0:
-            #    print(f"Epoch {epoch + 1} Batch {i} loss: {loss.item()}")
+            if i % 100 == 0:
+                print(f"Epoch {epoch + 1} Batch {i} loss: {loss.item()}")
 
         if epoch_loss / len(dataL) > prevLoss:
-            lossDec = False
+            if pat == 0:
+                lossDec = False
+            else:
+                pat -= 1
+        else:
+            pat = 5
 
-        print(f"Epoch {epoch + 1} loss: {epoch_loss / len(dataL)} | Change: {(epoch_loss / len(dataL)) - prevLoss}")
+        print(
+            f"Epoch {epoch + 1} loss: {epoch_loss / len(dataL)} | Change: {(epoch_loss / len(dataL)) - prevLoss}")
         logFile.write(
             f"Epoch {epoch + 1} loss: {epoch_loss / len(dataL)} | Change: {(epoch_loss / len(dataL)) - prevLoss}" + "\n")
+        logFile.flush()
         prevLoss = epoch_loss / len(dataL)
         epoch += 1
 
 
-if not os.path.exists("trainData.pkl"):
-    jsonInput = open('reviews_Movies_and_TV_processed.json', 'r', encoding="utf8")
+if not os.path.exists("trainData4.pkl"):
+    print("Dataset not found")
+    jsonInput = open('reviews_Movies_and_TV_processed.json',
+                     'r', encoding="utf8")
     curCount = 0
     totalSentences = []
     for line in jsonInput:
@@ -142,37 +182,17 @@ if not os.path.exists("trainData.pkl"):
         totalSentences.extend(json.loads(line)["reviewText"])
     jsonInput.close()
     trainData = Data(totalSentences)
-    pickle.dump(trainData, open("trainData.pkl", "wb"))
+    pickle.dump(trainData, open("trainData4.pkl", "wb"))
 else:
-    print("Loading data")
-    trainData = pickle.load(open("trainData.pkl", "rb"))
+    print("Dataset found")
+    trainData = pickle.load(open("trainData4.pkl", "rb"))
 
 print("Loaded")
 
 model = NN(500, trainData.vocab_size)
-if os.path.exists("modelFixed.pt"):
-    model.load_state_dict(torch.load("modelFixed.pt"))
+if os.path.exists("modelFixed500_4.pt"):
+    model.load_state_dict(torch.load("modelFixed500_4.pt"))
     print("Loaded model")
-    word2check = "director"
-    distances = {}
-    w2cOneHot = torch.zeros(trainData.vocab_size)
-    w2cOneHot[trainData.w2idx[word2check]] = 1
-    embedding = model.elayer(w2cOneHot.to(model.device))
-    wOneHot = torch.zeros(trainData.vocab_size)
-    for word in trainData.w2idx:
-        # distance from titanic
-        wOneHot[trainData.w2idx[word]] = 1
-        embedding2 = model.elayer(wOneHot.to(model.device))
-        wOneHot[trainData.w2idx[word]] = 0
-        distance = torch.dist(embedding, embedding2)
-        distances[word] = distance.item()
-
-    # top 10 words
-    for word in sorted(distances, key=distances.get)[:10]:
-        print(word, distances[word])
-
-    exit(0)
-
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 criterion = nn.CrossEntropyLoss()
 train(model, trainData, optimizer, criterion)
